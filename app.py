9d422cf7da67fe7d7792c15d7365a4d939592b3e
BrunoTemple <bruno.temple@gmail.com>
2023-04-13 13:31:18 -0400

    test subfolder

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import json
 import time
+from lib.pack import pack_fn
 
 app = Flask(__name__)
 
@@ -10,6 +11,13 @@ def home_page():
     data_set = {'Page': 'Home',
                 'Message': 'Loaded Home Page', 'Time': time.time()}
     json_dump = json.dumps(data_set)
+
+    return json_dump
+
+
+@app.route('/pack', methods=['GET'])
+def pack_page():
+    json_dump = pack_fn()
 
     return json_dump
 
